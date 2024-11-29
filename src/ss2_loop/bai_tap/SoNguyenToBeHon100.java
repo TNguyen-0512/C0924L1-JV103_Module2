517722181db730f7efac69f1c47c9fcb1f5268ca
Your Name <btn8176@gmail.com>
2024-11-29 19:51:22 +0700

    baitap

---
+++ src/ss2_loop/bai_tap/SoNguyenToBeHon100.java
@@ -0,0 +1,19 @@
+package ss2_loop.bai_tap;
+
+public class SoNguyenToBeHon100 {
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố < 100: ");
+        for( int number = 2; number <= 100; number++){
+            boolean isPrime = true;
+            for(int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(number);
+            }
+        }
+    }
+}
