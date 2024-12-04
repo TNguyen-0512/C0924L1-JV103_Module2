43bd14859854efb9c84d7049fd6a5054f169c223
Your Name <btn8176@gmail.com>
2024-12-04 19:12:47 +0700

    baitap

---
+++ src/ss4_class_oop/bai_tap/QuadraticEquation.java
@@ -0,0 +1,29 @@
+package ss4_class_oop.bai_tap;
+
+public class QuadraticEquation {
+    private double a;
+    private double b;
+    private double c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getDiscriminant() {
+        return b * b - 4 * a * c;
+    }
+}
