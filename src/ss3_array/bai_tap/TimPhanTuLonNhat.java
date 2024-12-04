5073a3ad343a3e537c95a4eb9d269ad57acf49f3
Your Name <btn8176@gmail.com>
2024-12-04 15:42:15 +0700

    baitap

--- src/ss3_array/bai_tap/TimPhanTuLonNhat.java
+++ src/ss3_array/bai_tap/TimPhanTuLonNhat.java
@@ -6,14 +6,14 @@ public class TimPhanTuLonNhat {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[][] matrix = {
-                {5,92,8},
-                {9,7,6},
-                {11,12,15},
+                {5, 92, 8},
+                {9, 7, 6},
+                {11, 12, 15},
         };
         int max = matrix[0][0];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] >max) {
+                if (matrix[i][j] > max) {
                     max = matrix[i][j];
                 }
             }
---
+++ src/ss4_class_oop/bai_tap/PhuongTringBac2.java
@@ -0,0 +1,12 @@
+package ss4_class_oop.bai_tap;
+
+import java.util.Scanner;
+
+public class PhuongTringBac2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+    private static void QuadraticEquation(double a, double b, double c) {
+
+    }
+}
