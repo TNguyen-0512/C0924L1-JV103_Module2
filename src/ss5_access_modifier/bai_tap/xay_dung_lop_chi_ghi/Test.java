c1146935089df1c548055a4386f0a85ae6566a91
Your Name <btn8176@gmail.com>
2024-12-09 18:30:17 +0700

    baitap

--- src/ss5_access_modifier/bai_tap/xay_dung_lop_chi_ghi/Test.java
+++ src/ss5_access_modifier/bai_tap/xay_dung_lop_chi_ghi/Test.java
@@ -6,13 +6,13 @@ public class Test {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Student student1 = new Student();
-        System.out.println("----Before set name and classes----");
+        System.out.println("----Tên và lớp trước khi đổi----");
         System.out.println(student1.toString());
-        System.out.println("----Set name----");
+        System.out.println("---Đổi Tên----");
         student1.setName(in.nextLine());
-        System.out.println("----Set classes----");
+        System.out.println("----Đổi Lớp----");
         student1.setClasses(in.nextLine());
-        System.out.println("----After set name and classes----");
+        System.out.println("----Tên và lớp sau khi đổi----");
         System.out.println(student1.toString());
     }
 }
