0352514ebc120d11a15821a349809390a8789055
Your Name <btn8176@gmail.com>
2024-12-09 20:09:51 +0700

    baitap

--- src/ss6_ke_thua/bai_tap/point2d_and_point3d/Run.java
+++ src/ss6_ke_thua/bai_tap/point2d_and_point3d/Run.java
@@ -3,8 +3,8 @@ package ss6_ke_thua.bai_tap.point2d_and_point3d;
 
 public class Run {
     public static void main(String[] args) {
-        Point2D point2D = new Point2D();
-        Point3D point3D = new Point3D();
+        Point2D point2D = new Point2D(6,5);
+        Point3D point3D = new Point3D(point2D.getX(), point2D.getY(), 6);
         System.out.println(point2D);
         System.out.println(point3D);
     }
--- src/ss6_ke_thua/bai_tap/point_and_movablepoint/Run.java
+++ src/ss6_ke_thua/bai_tap/point_and_movablepoint/Run.java
@@ -2,8 +2,8 @@ package ss6_ke_thua.bai_tap.point_and_movablepoint;
 
 public class Run {
     public static void main(String[] args) {
-        Point point = new Point();
-        MovablePoint movablePoint = new MovablePoint();
+        Point point = new Point(5.6F,7.5F);
+        MovablePoint movablePoint = new MovablePoint(point.getX(), point.getY(),5.5F,7.5F);
         System.out.println(point);
         System.out.println(movablePoint);
     }
