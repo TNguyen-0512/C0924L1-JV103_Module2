db557e9812dcd2cd5475610ee418fb5ca441c63c
Your Name <btn8176@gmail.com>
2024-12-18 20:35:48 +0700

    baitap

--- src/ss8_clear_code/bai_tap/service/ClientService.java
+++ src/ss8_clear_code/bai_tap/service/ClientService.java
@@ -6,10 +6,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class ClientService implements IClientService {
-    private static Client[] clients = new Client[10];
+    private static final Client[] clients = new Client[10];
 
     static {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         clients[0] = new Client(1, "Bùi Trung Nguyên", LocalDate.parse("05/12/2003", formatter), "Gia Lai");
         clients[1] = new Client(2, "Bùi Trung Nguyên", LocalDate.parse("05/12/2003", formatter), "Gia Lai");
         clients[2] = new Client(3, "Bùi Trung Nguyên", LocalDate.parse("05/12/2003", formatter), "Gia Lai");
@@ -19,11 +19,10 @@ public class ClientService implements IClientService {
     public Client[] findAll() {
         return clients;
     }
-
     @Override
     public void addClient(Client client) {
         for (int i = 0; i < clients.length; i++) {
-            if (clients[i] == null) {
+            if (clients[i]==null) {
                 clients[i] = client;
                 break;
             }
